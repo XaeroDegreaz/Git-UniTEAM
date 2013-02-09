b1e10a90a536817fc9c65f2b10f9d1a248b3de50
Jerome Doby <xaerodegreaz@gmail.com>
2013-02-08 21:21:25 -0600

    test

--- Assets/Git UniTEAM/Editor/FetchHelper.cs
+++ Assets/Git UniTEAM/Editor/FetchHelper.cs
@@ -24,7 +24,7 @@ namespace UniTEAM {
 						OnTransferProgress,
 					    credentials: creds
 					);
-
+					Console.branch = Console.repo.Head;
 					isFetchComplete = true;
 				});
 			} catch ( System.Exception e ) {
